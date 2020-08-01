70fe502e11ac3007834e37f095655d4487ec1fdd
Brianna Bullock <brianna.bullock16@gmail.com>
2020-07-31 23:12:26 -0500

    added roles

--- seed.sql
+++ seed.sql
@@ -7,6 +7,17 @@ VALUES
 ("engineering"),
 ("research")
 
+INSERT INTO role (title, salary, department_id) 
+VALUES
+("project manager", 78000, 1),
+("content strategist", 65000, 1),
+("tax accountant", 85000, 2),
+("finance manager", 85000, 2),
+("lead engineer", 82000, 3),
+("general engineer", 82000, 3),
+("research assistant", 35000, 4),
+("lab manager", 75000, 4);
+
 
 
 
