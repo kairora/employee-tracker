d3d709ac043423b8badc97417522572a7c0f30d8
Brianna Bullock <brianna.bullock16@gmail.com>
2020-08-01 17:43:49 -0500

    can view all employees with dept and manager etc

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+const cTable = require('console.table');
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -42,16 +43,21 @@ var connection = mysql.createConnection({
           "Remove a department",
           "Update an employee role",
           "Update an employee manager",
-          "exit"        
+          "Exit"        
         ]
       })
       .then(function(answer) {
         switch (answer.action) {
         case "View all employees":
-          showAll();
+          allEmployees();
           break;
   
         // case "View employees by department":
+          // empByDept();
+
+          // break;
+
+        // case "View employees by manager":
           // empByDept();
           // break;
   
@@ -62,6 +68,43 @@ var connection = mysql.createConnection({
         case "View all departments":
           allDepts();
           break;
+        
+        // case "View total budget by department":
+          // empByDept();
+          // break;
+        
+        case "Add an employee":
+          addEmployee();
+          break;
+
+        case "Add a department":
+          addDept();
+          break;
+
+        // case "Add a role":
+          // empByDept();
+          // break;
+
+        // case "Remove an employee":
+          // empByDept();
+          // break;
+
+        // case "Remove a role":
+          // empByDept();
+          // break;
+
+        case "Remove a department":
+          deleteDept();
+          break;
+
+        // case "Update an employee role":
+          // empByDept();
+          // break;
+
+        // case "Update an employee manager":
+          // empByDept();
+          // break;
+        
   
         case "exit":
           connection.end();
@@ -70,11 +113,26 @@ var connection = mysql.createConnection({
       });
   }
   
-  function showAll() {
-    console.log("Searching for all employees...")
-    connection.query("SELECT * FROM employee", function(err, results) {
+  // get by ID
+  function roleID(answer) {
+    
+    connection.query('SELECT id FROM role WHERE title = ?', answer.title, function(err, result) {
       if (err) throw err;
-      console.table(results)
+      return result
+  });
+  }
+
+
+  // View All
+  function allEmployees() {
+    console.log("Searching for all employees...")
+    connection.query(`SELECT employee.id, employee.first_name First_Name, employee.last_name Last_Name, title Role, Salary, department.name Department, CONCAT_WS(' ', e.first_name,  e.last_name) Manager FROM employee 
+    LEFT JOIN role ON employee.role_id = role.id
+    LEFT JOIN department ON role.department_id = department.id
+    LEFT JOIN employee e ON employee.manager_id = e.id;`, function(err, results) {
+      if (err) throw err;
+      const table = cTable.getTable(results)
+      console.log(table);
       startPrompts();
   });
   }
@@ -83,19 +141,96 @@ var connection = mysql.createConnection({
     console.log("Grabbing all the roles...")
     connection.query('SELECT id AS "ID", title AS "Title", salary AS "Salary" FROM role', function(err, results) {
       if (err) throw err;
-      console.table(results)
+      const table = cTable.getTable(results)
+      console.log(table);
       startPrompts();
   });
   }
 
-  function allDepts() {
-    console.log("Displaying all the departments...")
-    connection.query('SELECT * FROM department', function(err, results) {
-      if (err) throw err;
-      console.table(results)
+
+
+
+  // ADD
+  // function addEmployee() {
+  //   inquirer
+  //   .prompt([
+  //     {
+  //       name: "first_name",
+  //       type: "input",
+  //       message: "What is the employee's first name? ",
+  //       validate: function(value) {
+  //         if(input !== "") {
+  //           return true;
+  //         } else {
+  //           return "Please input the employee's first name."
+  //         }
+  //       }
+  //     },
+  //     {
+  //       name: "last_name",
+  //       type: "input",
+  //       message: "What is the employee's last name? ",
+  //       validate: function(value) {
+  //         if(input !== "") {
+  //           return true;
+  //         } else {
+  //           return "Please input the employee's last name."
+  //         }
+  //       }
+  //     },
+  //     {
+  //       name: "role",
+  //       type: "input",
+  //       message: "What is the employee's role? ",
+  //       validate: function(value) {
+  //         if(input !== "") {
+  //           return true;
+  //         } else {
+  //           return "Please input the employee's last name."
+  //         }
+  //       }
+  //     },
+  //   ])
+  //   .then(answers => {
+  //     let newRoleID = roleID();
+  //     connection.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ? ? ? ?', answers.first_name, answers.last_name, newRoleID,  function(err, results) {
+  //       if (err) throw err;
+  //       const table = cTable.getTable(allDepts(results)
+  //       console.log(table);
+  //       startPrompts();
+  //     }) 
+  //   })
+  // }
+
+
+  function addDept() {
+    inquirer
+    .prompt([
+      {
+        name: "dept_name",
+        type: "input",
+        message: "What department would you like to add? "
+      },
+    ])
+    .then(answer => {
+      connection.query('INSERT INTO department (name) VALUES (?)', answer.dept_name,  function(err, results) {
+        if (err) throw err;
+        console.log(answer.dept_name)
+        const table = cTable.getTable(allDepts(results))
+        console.log(table);
+      }) 
       startPrompts();
-  });
+    })
   }
+
+  // function empByDept() {
+  //   onsole.log("Displaying all the departments...")
+  //   connection.query('SELECT * FROM department', function(err, results) {
+  //     if (err) throw err;
+  //     const table = cTable.getTable(results)
+      // console.log(table);
+  //     startPrompts();
+  // }
 
   // function deptSearch() {
   //   console.log("Seaching for employee by department...")
@@ -112,7 +247,8 @@ var connection = mysql.createConnection({
   //       }, 
   //       (err, results) => {
   //           if(err) throw err
-  //           console.table(results)
+  //           const table = cTable.getTable(results)
+      // console.log(table);
   //           initialPrompts()
   //       })
   //   })
