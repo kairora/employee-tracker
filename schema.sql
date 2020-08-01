bcc71bece12a693937cf6ddf7f740e9848a7a479
Brianna Bullock <brianna.bullock16@gmail.com>
2020-07-31 21:45:36 -0500

    created employee table

--- schema.sql
+++ schema.sql
@@ -17,3 +17,13 @@ CREATE TABLE role (
     CONSTRAINT fk_dept_id FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
+CREATE TABLE employee(
+	id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT UNSIGNED,
+    manager_id INT UNSIGNED,
+    CONSTRAINT fk_role_id FOREIGN KEY (role_id) REFERENCES role(id),
+    CONSTRAINT fk_man_id FOREIGN KEY (manager_id) REFERENCES employee(id)
+)
+
